6fb266379403fc2ffc002182969f1f3e7123eacd
leejss <goldemshine@gmail.com>
2022-10-14 19:42:49 +0900

    daily leetcode

--- src/leetcode/isIsomorphic.ts
+++ src/leetcode/isIsomorphic.ts
@@ -1,23 +1,44 @@
 // https://leetcode.com/problems/isomorphic-strings/?envType=study-plan&id=level-1
 function isIsomorphic(s: string, t: string): boolean {
-  // occurrance map
-  // keep order
+  if (s.length !== t.length) return false;
+  let result = true;
 
   const counter = (arr: string) => {
-    return Array.from(arr).reduce((prev, cur) => {
-      if (!(cur in prev)) {
-        prev[cur] = 1;
-      } else {
-        prev[cur] += 1;
+    const m: Record<string, number> = {};
+
+    for (let i = 0; i < arr.length; i++) {
+      if (!(arr[i] in m)) {
+        m[arr[i]] = i;
       }
-      return prev;
-    }, {} as Record<string, number>);
+    }
+
+    return m;
   };
 
-  const counterSLength = Object.keys(counter(s)).length;
-  const counterTLength = Object.keys(counter(t)).length;
+  const sm = counter(s);
+  const tm = counter(t);
 
-  return counterSLength !== counterTLength ? false : true;
+  const sr = Array.from(s).map((char) => sm[char]);
+  const tr = Array.from(t).map((char) => tm[char]);
+
+  for (let i = 0; i < sr.length; i++) {
+    if (sr[i] !== tr[i]) {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
 }
 
-isIsomorphic("badc", "baba");
+console.log(isIsomorphic("foo", "bar"));
+
+// foo => 011
+// bar => 012
+
+// { 0: f, 1: 0}
+
+// bar
+// {0 :b, 1: a, 2: r}
+
+//
--- src/leetcode/isSubsequence.ts
+++ src/leetcode/isSubsequence.ts
@@ -1,2 +1,10 @@
 // https://leetcode.com/problems/is-subsequence/?envType=study-plan&id=level-1
-function isSubsequence(s: string, t: string): boolean {}
+function isSubsequence(s: string, t: string): boolean {
+  if (s.length > t.length) return false;
+  let count = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[count] === t[i]) count++;
+  }
+  return count === s.length;
+}
+console.log(isSubsequence("abc", "ahbgdc"));
